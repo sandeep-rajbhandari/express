8f8da593a473090ba3b204068e07dbfa03547cd3
srajbhandari <srajbhandari@deerwalk.com>
2014-11-28 17:28:44 +0545

    sandeep

--- expressTest/routes/index.js
+++ expressTest/routes/index.js
@@ -1,17 +1,9 @@
 var express = require('express');
-var app = module.exports = express();
 var router = express.Router();
 
 /* GET home page. */
-app.engine('.html', require('jade').__express);
-app.set('views', __dirname + '/views');
-
-// Without this you would need to
-// supply the extension to res.render()
-// ex: res.render('users.html').
-app.set('view engine', 'html');
 router.get('/', function(req, res) {
-  res.render('index');
+  res.render('test');
 });
 
 module.exports = router;
